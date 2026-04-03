50d43ffb3878bf7e7a1b27862a910d4498adf6d7
ahmed12106350 <149765253+ahmed12106350@users.noreply.github.com>
2026-04-03 16:51:40 +0530

    Challenge-Adding Post request

--- Horizon-Project/Horizon-Challenge-5/handlers/routeHandlers.js
+++ Horizon-Project/Horizon-Challenge-5/handlers/routeHandlers.js
@@ -1,13 +1,19 @@
 // handleGet
-
+import { sendResponse } from '../utils/sendResponse.js'
+import { getData } from '../utils/getData.js'
+export async function handleGet(res) {
+   const data = await getData()
+   const parsedData = JSON.stringify(data)
+   sendResponse(res, 200, 'application/json', parsedData)
+}
 /*
 Challenge:
-1. Export a function called handleGet(). 
+1. Export a function called handleGet().
 2. It should:
    - use getData() to get the data
    - stringify that data
    - use sendResponse() to serve it
-   
+
 Open the browser and load the sightings page to see if it works.
 */
 
--- Horizon-Project/Horizon-Challenge-6/server.js
+++ Horizon-Project/Horizon-Challenge-6/server.js
@@ -7,16 +7,16 @@ const PORT = 8000
 const __dirname = import.meta.dirname
 
 const server = http.createServer(async (req, res) => {
+
+    if (req.url === '/api') {
+        if (req.method === 'GET') {
+            return await handleGet(res)
+        }
 /*
 Challenge: 
-   1. Set up a route for ‘/api’.
-   2. Nest an if to check if the method is ‘GET’. 
-   3. When a GET request is received to '/api', use handleGet() to handle it.
-*/ if (req.url == '/api') {
-        if (req.method == 'GET') {
-            const data = await handleGet(res)
-            return data
-        }
+   1. Add a route for a POST request to '/api'.
+   2. When a request comes in, pass the req and res to handlePost().
+*/
     }
     else if (!req.url.startsWith('/api')) {
         return await serveStatic(req, res, __dirname)
